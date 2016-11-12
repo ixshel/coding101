0b7e845a97c25d7de88bec5874c4d1fbe961582e
ixshel escamilla <ixshel@ixshels-MacBook-Pro.local>
2016-11-12 12:30:32 -0600

    start

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-
-
-
-
-var express = requiere('express');
-var path = requiere('path');
-var logger = requiere('morgan');
-var bodyParse = requiere('body-parser');
-
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var assistants = require('./routes/assistants');
 var app = express();
 
-// Endpoints
-/*
-app.use(logger('dev');
-
-app.use(bodyParse.json()));
-app.use(bodyParse.urlencoded({
+//Endpoint url
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/')
-*/
+app.use('/', assistants);
 
-var server = app.liste(process.env.Port || 3000, function(){
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+var server = app.listen(process.env.PORT || 3000, function() {
     var host = 'localhost';
-    var port = server.address().portconsole.log('app runnung...');
+    var port = server.address().port;
+    console.log('App listening at http://%s:%s', host, port);
 });
 
 module.exports = app;

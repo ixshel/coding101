b757f2f63eb631bbb9f90c391f5b7b1ab2b7d28d
ixshel escamilla <ixshel@ixshels-MacBook-Pro.local>
2016-11-12 11:45:17 -0600

    aa

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 
 
 
-var express = require('express');
+var express = requiere('express');
+var path = requiere('path');
+var logger = requiere('morgan');
+var bodyParse = requiere('body-parser');
 
 var app = express();
 
-console.log('express loaded')
+// Endpoints
+/*
+app.use(logger('dev');
+
+app.use(bodyParse.json()));
+app.use(bodyParse.urlencoded({
+    extended: false
+}));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/')
+*/
+
+var server = app.liste(process.env.Port || 3000, function(){
+    var host = 'localhost';
+    var port = server.address().portconsole.log('app runnung...');
+});
+
+module.exports = app;
+

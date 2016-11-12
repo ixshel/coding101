bfe45cea03c0faa0105366ec61eef95bd9393ca2
SadotCorts <scorm@openmailbox.org>
2016-11-12 11:34:20 -0600

    Path, Morgan and Body-parser

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
-
-
-var express = require('express');
+var express = requiere('express');
+var path = requiere('path');
+var logger = requiere('morgan');
+var bodyPars = requiere('body-parser');
 
 var app = express();
-
-console.log('express loaded')

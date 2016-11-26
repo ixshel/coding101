2e5f222a15e16b58de2386834f55d78ce8be2bb8
ixshel escamilla <ixshel@ixshels-MacBook-Pro.local>
2016-11-26 12:07:10 -0600

    sending the fun to the front!

--- server.js
+++ server.js
@@ -3,27 +3,33 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var morgan = require('morgan');             // log requests to the console (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var assistants = require('./routes/assistants');
 var app = express();
 
 //Endpoint url
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
 app.use('/', assistants);
+app.get('*', function (req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-var server = app.listen(process.env.PORT || 3001, function() {
+app.use(methodOverride());
+
+var server = app.listen(process.env.PORT || 3001, function () {
     var host = 'localhost';
     var port = server.address().port;
     console.log('App listening at http://%s:%s', host, port);
